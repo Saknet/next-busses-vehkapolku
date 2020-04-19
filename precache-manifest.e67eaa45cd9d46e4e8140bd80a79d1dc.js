982a6040929047b689e727c923f482a32abda35b
Saknet <andreas.andra@helsinki.fi>
2020-04-19 18:00:43 +0300

    Updates

---
+++ precache-manifest.e67eaa45cd9d46e4e8140bd80a79d1dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20a44faa3aea18dee63f1c43d8de1b1",
+    "url": "/next-busses-vehkapolku/index.html"
+  },
+  {
+    "revision": "535aa57e137a66c68832",
+    "url": "/next-busses-vehkapolku/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "59a21758fdd86aa66552",
+    "url": "/next-busses-vehkapolku/static/js/2.ecab3b57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/next-busses-vehkapolku/static/js/2.ecab3b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535aa57e137a66c68832",
+    "url": "/next-busses-vehkapolku/static/js/main.b711f6cf.chunk.js"
+  },
+  {
+    "revision": "aecc039e3573fdd035ae",
+    "url": "/next-busses-vehkapolku/static/js/runtime-main.af8dd0fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/next-busses-vehkapolku/static/media/logo.5d5d9eef.svg"
+  }
+]);
